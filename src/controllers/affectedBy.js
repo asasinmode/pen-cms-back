edafb95756971800bd4ad02b3025ac70617953f2
asasinmode <skin9470@gmail.com>
2022-08-14 16:29:45 +0200

    affectedBy functionality

--- src/controllers/affectedBy.js
+++ src/controllers/affectedBy.js
@@ -1,4 +1,11 @@
 import expressAsyncHandler from "express-async-handler"
+import PenModel from "../database/models/Pen.js"
+
+const affectedByDeletionNumber = (path) => {
+   return PenModel.countDocuments({
+      [path]: { $exists: true }
+   })
+}
 
 const getAffectedByDeletion = expressAsyncHandler(async (req, res) => {
    const properties = req.query.property
@@ -10,16 +17,19 @@ const getAffectedByDeletion = expressAsyncHandler(async (req, res) => {
    const propertiesArray = properties.split(",")
 
    const rv = propertiesArray ?
-      propertiesArray.reduce((previous, current) => ({
-         ...previous,
-         [current]: Math.floor(Math.random() * (100 - 0 + 1)) + 0
-      }), {})
+      await propertiesArray.reduce(async (previous, current) => {
+         const propertyPath = `properties.${ current }`
+         return {
+            ...previous,
+            [current]: await affectedByDeletionNumber(propertyPath)
+         }
+      }, {})
       : {}
 
    res.json(rv)
 })
 
-const getAffectedByUpdate = expressAsyncHandler((req, res) => {
+const getAffectedByUpdate = expressAsyncHandler(async (req, res) => {
    const property = req.query.property
    if(!property){
       res.status(400).send("query parameter \"property\" is required")
@@ -32,17 +42,26 @@ const getAffectedByUpdate = expressAsyncHandler((req, res) => {
 
    const rv = {}
 
+   const propertyPath = `properties.${ property }`
+   const pensWithProperty = await PenModel.find({
+      [propertyPath]: { $exists: true }
+   }).select({ properties: 1, _id: 0 })
+
    if(updatedValues && updatedValues.length > 0){
-      rv.updated = updatedValues.reduce((previous, current) => ({
-         ...previous,
-         [current]: Math.floor(Math.random() * (100 - 0 + 1)) + 0
-      }), {})
+      rv.updated = updatedValues.reduce((previous, current) => {
+         return {
+            ...previous,
+            [current]: pensWithProperty.filter(pen => pen.properties[property] === current).length
+         }
+      }, {})
    }
    if(deletedValues && deletedValues.length > 0){
-      rv.deleted = deletedValues.reduce((previous, current) => ({
-         ...previous,
-         [current]: Math.floor(Math.random() * (100 - 0 + 1)) + 0
-      }), {})
+      rv.deleted = deletedValues.reduce((previous, current) => {
+         return {
+            ...previous,
+            [current]: pensWithProperty.filter(pen => pen.properties[property] === current).length
+         }
+      }, {})
    }
 
    res.json(rv)
