bc06ed0efc4a4ec4674dc1f621776264eddb6a16
asasinmode <skin9470@gmail.com>
2022-08-14 20:43:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ import app from "./src/app.js"
 const port = process.env.PORT || 8000
 
 app.listen(port, () => {
-   console.log("listening on http://localhost:8000/")
+   console.log(`listening on http://localhost:${ port }/`)
 })
